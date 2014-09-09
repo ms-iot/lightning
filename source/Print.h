8495529c07653f7416470890c2367fad8ddd8d60
Nicolas BESSON <nbesson@adeneo-embedded.com>
2014-09-09 09:57:25 +0200

    Fixed multi inclusion of Print.h by adding #ifdef

--- source/Print.h
+++ source/Print.h
@@ -5,6 +5,8 @@
 // Print.h contains functionality for print functions
 // virtual size_t write(uint8_t) needs to be overwritten in any extension of Print.h
 // Info on fundamental types found here: http://msdn.microsoft.com/en-us/library/cc953fe1.aspx
+#ifndef _PRINT_H
+#define _PRINT_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -359,3 +361,4 @@ public:
 	// prints a new line character
 	size_t println(void) { return print('\n'); }
 };
+#endif // _PRINT_H
