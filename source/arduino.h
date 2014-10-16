4c00d45398df559c568df08a9427c3d0c8a59ba8
Zachary J. Fields <zachary_fields@yahoo.com>
2014-10-16 10:19:16 -0700

    Replace Arduino supplied values with math.h values. - PI, TWO_PI, HALF_PI

--- source/arduino.h
+++ source/arduino.h
@@ -94,9 +94,9 @@
 #define WLED            (QRK_LEGACY_RESUME_SUS1)
 #define LED_BUILTIN     WLED
 
-#define PI              3.1415926535897932384626433832795
-#define HALF_PI         1.5707963267948966192313216916398
-#define TWO_PI          6.283185307179586476925286766559
+#define PI              M_PI
+#define HALF_PI         M_PI/2
+#define TWO_PI          M_PI*2
 
 #define boolean bool
 typedef uint8_t byte;
